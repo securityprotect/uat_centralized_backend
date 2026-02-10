9ca2f949f7e564204320972a742d0f959196d40e
ParkPing <105095888+securityprotect@users.noreply.github.com>
2026-02-10 18:54:13 +0530

    Delete api directory

--- api/admin/submissions.js
+++
@@ -1,8 +0,0 @@
-import connectDB from "../../lib/db";
-import Submission from "../../models/Submission";
-
-export default async function handler(req, res) {
-  await connectDB();
-  const data = await Submission.find({ paymentStatus: "SUCCESS" });
-  res.json(data);
-}
