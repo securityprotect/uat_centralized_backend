b53839fc2c2c276f161c15f0494b189e13d6f88a
ParkPing <105095888+securityprotect@users.noreply.github.com>
2026-02-10 17:03:19 +0530

    Update apply.js

--- api/apply.js
+++ api/apply.js
@@ -1,20 +1,14 @@
-import connectDB from "../lib/db";
+import connectDB from "../lib/mongo";
 import Submission from "../models/Submission";
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).end();
 
   await connectDB();
 
   const submission = await Submission.create(req.body);
 
-  res.json({
-    success: true,
+  return res.json({
     submissionId: submission._id
   });
 }
